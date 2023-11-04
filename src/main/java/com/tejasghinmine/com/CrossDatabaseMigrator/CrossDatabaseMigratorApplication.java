f6d7d5a45b93ba1f2491c77c9d3eecc1bb41d8ba
Tejas Ghinmine <tejasghinmine009@gmail.com>
2023-11-04 18:28:32 +0530

    first commit

---
+++ src/main/java/com/tejasghinmine/com/CrossDatabaseMigrator/CrossDatabaseMigratorApplication.java
@@ -0,0 +1,13 @@
+package com.tejasghinmine.com.CrossDatabaseMigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossDatabaseMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossDatabaseMigratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejasghinmine/com/CrossDatabaseMigrator/CrossDatabaseMigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejasghinmine.com.CrossDatabaseMigrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossDatabaseMigratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
