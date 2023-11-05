d20ab7391ae5920ca4e13187533bb6cdb770ae2f
Tejas Ghinmine <tejasghinmine009@gmail.com>
2023-11-05 23:11:48 +0530

    completing the core structure except script generator

--- src/main/java/com/tejasghinmine/com/CrossDatabaseMigrator/util/ScriptGenerator.java
+++ src/main/java/com/tejasghinmine/com/CrossDatabaseMigrator/util/ScriptGenerator.java
@@ -1,8 +1,10 @@
 package com.tejasghinmine.com.CrossDatabaseMigrator.util;
 
 import com.tejasghinmine.com.CrossDatabaseMigrator.entity.MigrateDbRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+@Component
 public class ScriptGenerator {
 
     public String generateScript(MigrateDbRequest migrateDbRequest, MultipartFile extractTable, MultipartFile insertTable, MultipartFile queryTable) {
